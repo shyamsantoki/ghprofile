c4e6395799d8129a2a21275c3e0749ce60f7c771
shyamsantoki <shyam_santoki@outlook.com>
2022-05-21 11:57:43 +0530

    github profile card completed

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import styles from "./Users.module.css";
+import style from "./Users.module.css";
 
 export default function DisplayUser() {
 	const [details, setDetails] = useState([]);
@@ -18,62 +18,31 @@ export default function DisplayUser() {
 	};
 	useEffect(() => getDetails(), []);
 	return (
-		<div className={styles.users}>
-			<img
-				src={details.avatar_url}
-				className={styles.avatar_url}
-				alt="profile pic"
-			></img>
-			<div className={styles.flexb}>
-				<div className={styles.name}>{details.name}</div>
-				<div className={styles.login}>{details.login}</div>
+		<div className={style.users}>
+			<div className={style.profile_details}>
+				<img className={style.avatar_url} src={details.avatar_url} alt="profile"/>
+				<div className={style.namewrapper}>
+					<div className={style.name}>{details.name}</div>
+					<div className={style.login}>{details.login}</div>
+				</div>
 			</div>
-			<div className={styles.bio}>{details.bio}</div>
-			<div className={styles.public_repos}>{details.public_repos}</div>
-			<div className={styles.created_at}>{details.created_at}</div>
-			<div className={styles.updated_at}>{details.updated_at}</div>
-			<div className={styles.blog}>{details.blog}</div>
-			<div className={styles.location}>{details.location}</div>
-			<div className={styles.email}>{details.email}</div>
-		</div>
+			<div className={style.bio}>{details.bio}</div>
+			<div className={`${style.continfo} ${style.icon}`}>
+				<i className="fa fa-folder" aria-hidden="true"><span className={style.extraspace}>{details.public_repos}</span></i>
+				<i className="fa fa-file" aria-hidden="true"><span className={style.extraspace}>{details.public_gists}</span></i>
+			</div>
+			<div className={`${style.text} ${style.icon}`}>
+				<i className="fa-solid fa-location-pin"><span className={`${style.text} ${style.extraspace}`}>{details.location === null ? "N/A" : details.location}</span></i>
+			<div className={`${style.text} ${style.icon}`}>
+				<i className="fa fa-globe" aria-hidden="true"><span className={`${style.text} ${style.extraspace}`}>{details.blog === "" ? "N/A" : details.blog}</span></i>
+			</div>
+			<div className={`${style.text} ${style.icon}`}>
+				<i className="fa-solid fa-at" aria-hidden="true"><span className={`${style.text} ${style.extraspace}`}>{details.email === null ? "N/A" : details.email}</span></i>
+			</div>
+			<div className={`${style.text} ${style.icon}`}>
+				<div className={style.created_at}>Created at: {details.created_at}</div>
+				<div className={style.updated_at}>Updatd at: {details.updated_at}</div>
+			</div>
+		</div></div>
 	);
-	{
-		/*
-			<html>
-				<head>
-					<link rel="stylesheet" href="stylesheet.css">
-					<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-					<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Plus+Jakarta+Sans">
-				</head>
-				<body>
-					<div class="users">
-						<div class="profile_details">
-							<img class="avatar_url" src="https://avatars.githubusercontent.com/u/56479219?v=4" alt="profile">
-							<div class="namewrapper">
-								<div class="name">Shyam Santoki</div>
-								<div class="login">shyamsantoki</div>
-							</div>
-						</div>
-						<div class="bio">You know who I am</div>
-						<div class="continfo icon">
-							<i class="fa fa-folder" aria-hidden="true"><span class="extraspace">16</span></i>
-							<i class="fa fa-file" aria-hidden="true"><span class="extraspace">0</span></i>
-						</div>
-						<div class="text icon">
-							<i class="fa-solid fa-location-pin"><span class="text extraspace">Surat, IN</span></i>
-						<div class="text icon">
-							<i class="fa fa-globe" aria-hidden="true"><span class="text extraspace">https://shyam.sh</span></i>
-						</div>
-						<div class="text icon">
-							<i class="fa-solid fa-at" aria-hidden="true"><span class="text extraspace">shyam_santoki@outlook.com</span></i>
-						</div>
-						<div class="times text">
-							<div class="created_at">Created: 2022-05-20</div>
-							<div class="updated_at">Updated: 2022-05-20</div>
-						</div>
-					</div>
-				</body>
-			</html>
-		*/
-	}
 }
